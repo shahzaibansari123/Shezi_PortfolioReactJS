7ebffc2d0fc550859a91537669174a58ad64b20f
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-16 04:30:47 +0500

    work complete

--- react_frontend/src/components/Navbar/Navbar.js
+++ react_frontend/src/components/Navbar/Navbar.js
@@ -6,14 +6,14 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.shahzaibpng} alt="logo" />
+        <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {['Home', 'About', 'Work', 'Skills','Contact'].map((item)=>(
+        {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item)=>(
           <li className="app__flex p-text" key={`link-${item}`}>
-            <div>
+            <div />
               <a href={`#${item}`}>{item}</a>
-            </div>
+    
           </li>
         ))}
       </ul>
