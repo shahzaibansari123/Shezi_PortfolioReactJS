ed7b941f17da1eeb90b6f2648f30479179ee83d4
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-19 04:36:13 +0500

    work complete

--- react_frontend/src/container/About/About.js
+++ react_frontend/src/container/About/About.js
@@ -22,6 +22,20 @@ const About = () => {
     </h2>
 
     <div className="app__profiles">
+      {abouts.map((about, index)=>(
+        <motion.div
+        whileInView={{opacity: 1}}
+        whileHover={{scale: 1.1}}
+        transition={{ duration: 0.5, type: 'tween'}}
+        className="app__profile-item"
+        key={about.title + index}
+        >
+          <img src={about.imgUrl} alt={about.title} />
+          <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
+          <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
+
+        </motion.div>
+      ))}
 
     </div>
     
