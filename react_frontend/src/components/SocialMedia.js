86fc1f70f051afcb2ede8c7f1fc99aa00767cae1
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-19 07:21:09 +0500

    work complete

---
+++ react_frontend/src/components/SocialMedia.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {BsInstagram, BsLinkedin} from 'react-icons/bs'
+import {FaFacebookF} from 'react-icons/fa'
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+        <div>
+            <BsLinkedin />
+        </div>
+        <div>
+            <BsInstagram />
+        </div>
+
+        <div>
+            <FaFacebookF />
+        </div>
+
+    </div>
+  )
+}
+
+export default SocialMedia
---
+++ react_frontend/src/wrapper/AppWrap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SocialMedia, NavigationDots} from '../components'
+
+const AppWrap = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppWrap
