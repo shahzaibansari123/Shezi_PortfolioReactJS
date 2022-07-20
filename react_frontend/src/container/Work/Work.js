574d4c66c80f011be5dd84cbd59aa7e0af24ae34
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-20 19:57:38 +0500

    work complete

--- react_frontend/src/container/Work/Work.js
+++ react_frontend/src/container/Work/Work.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {motion} from 'framer-motion'
+import {AiFillGithub, AiFillEye} from 'react-icons/ai'
+import {AppWrap} from '../../wrapper'
+import {urlFor, client} from '../../client'
 
 import './Work.scss'
 
 const Work = () => {
+
+  const handleWorkFilter=(item)=>{
+
+  }
   return (
-    <div>Work</div>
+    <>
+    <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+    <div className="app__work-filter">
+      {['UI/UX','Web App','Mobile App','All', 'ReactJS'].map((item, index)=>(
+        <div 
+        key={index}
+        onClick={()=> handleWorkFilter(item)}
+        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ' '}`}>
+
+
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
