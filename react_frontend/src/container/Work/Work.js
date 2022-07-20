c030903aeb2b327da55dfbfb6913e2660353ff90
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-20 20:23:47 +0500

    work complete

--- react_frontend/src/container/Work/Work.js
+++ react_frontend/src/container/Work/Work.js
@@ -8,6 +8,18 @@ import "./Work.scss";
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
 
   const handleWorkFilter = (item) => {};
   return (
@@ -29,6 +41,19 @@ const Work = () => {
           )
         )}
       </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className={"app__work-portfolio"}
+      >
+        {filterWork.map((work, index)=>(
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(imgUrl)} alt="img" />
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </>
   );
 };
