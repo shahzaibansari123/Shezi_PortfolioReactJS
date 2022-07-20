7b2b589d852734cc52f2ee40fe0bc09608f1a703
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-20 20:01:31 +0500

    work complete

--- react_frontend/src/container/Work/Work.js
+++ react_frontend/src/container/Work/Work.js
@@ -1,32 +1,36 @@
-import React, {useState, useEffect} from 'react'
-import {motion} from 'framer-motion'
-import {AiFillGithub, AiFillEye} from 'react-icons/ai'
-import {AppWrap} from '../../wrapper'
-import {urlFor, client} from '../../client'
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { AiFillGithub, AiFillEye } from "react-icons/ai";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
-import './Work.scss'
+import "./Work.scss";
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState("All");
 
-  const handleWorkFilter=(item)=>{
-
-  }
+  const handleWorkFilter = (item) => {};
   return (
     <>
-    <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
-    <div className="app__work-filter">
-      {['UI/UX','Web App','Mobile App','All', 'ReactJS'].map((item, index)=>(
-        <div 
-        key={index}
-        onClick={()=> handleWorkFilter(item)}
-        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ' '}`}>
-
-
-        </div>
-      ))}
-    </div>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> Section
+      </h2>
+      <div className="app__work-filter">
+        {["UI/UX", "Web App", "Mobile App", "ReactJS", "All"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text 
+              ${activeFilter === item ? "item-active" : " "}`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
