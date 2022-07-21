7a5c2056aefe5e927a6b11aee86cc380e81cf582
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-21 05:47:24 +0500

    work complete

--- react_frontend/src/container/Work/Work.js
+++ react_frontend/src/container/Work/Work.js
@@ -60,7 +60,7 @@ const Work = () => {
                 }}
                 className="app__work-hover app__flex"
               >
-                <a href="work.projectLink" target="_blank" rel="noreferrer">
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -70,7 +70,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href="work.codeLink" target="_blank" rel="noreferrer">
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
