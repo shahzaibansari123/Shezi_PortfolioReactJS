68624ffe82490f613e6c761be2ec3065ec66c7bf
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-22 13:04:02 +0500

    work complete

--- react_frontend/src/container/Skills/Skills.js
+++ react_frontend/src/container/Skills/Skills.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { AiFillGithub, AiFillEye } from "react-icons/ai";
+import ReactTooltip from 'react-tooltip'
 import { AppWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
-import "./Work.scss";
+import "./Skills.scss";
 
 const Skills = () => {
   return (
