906ac192f771c194381c5a92d61f4419603b908f
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-23 23:17:35 +0500

    work complete

--- react_frontend/src/container/Skills/Skills.js
+++ react_frontend/src/container/Skills/Skills.js
@@ -7,12 +7,30 @@ import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
+  
+  const [experience, setExperience]=useState([])
+  const [skills,setSkills]=useState([])
+  
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
   return 
   <>
   <h2 className="head-text">Skills and Experience</h2>
   <div className="app__skills-container">
-    <motion.div className="app">
+    <motion.div className="app__skills-list">
 
+{/* {skills.map} */}
     </motion.div>
 
   </div>
