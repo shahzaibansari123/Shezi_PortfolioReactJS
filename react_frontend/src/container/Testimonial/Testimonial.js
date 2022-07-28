15f82b8488adbc97b258cf494bd4cff1492e37d8
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-07-28 14:56:22 +0500

    work complete

--- react_frontend/src/container/Testimonial/Testimonial.js
+++ react_frontend/src/container/Testimonial/Testimonial.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
+import {motion} from 'framer-motion'
+import {HiChevronLeft,HiChevronRight} from 'react-icons/hi'
+
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import './Testimonial.scss'
 
 const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query).then((data) => {
+      // console.log(data)
+      setTestimonials(data);
+    });
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
   return (
     <div>
       Testimonial
@@ -9,4 +32,9 @@ const Testimonial = () => {
   )
 }
 
-export default Testimonial
+
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'), 
+  'Testimonial', 
+  "app__primarybg"
+  );
