1d2ad5480099302a6edb3d6ae08e6007a94dd663
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-08-01 21:55:14 +0500

    work complete

--- react_frontend/src/container/Footer/Footer.js
+++ react_frontend/src/container/Footer/Footer.js
@@ -12,15 +12,13 @@ const Footer = () => {
     email: "",
     message: "",
   });
-
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { name, email, message } = formData;
+  const { username, email, message } = formData;
 
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
-
     setFormData({ ...formData, [name]: value });
   };
 
@@ -29,50 +27,50 @@ const Footer = () => {
 
     const contact = {
       _type: "contact",
-      name: "name",
-      email: "email",
-      message: "message",
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
     };
 
-    client.create(contact).then(() => {
-      setLoading(false);
-      setIsFormSubmitted(true)
-      // setFormData(true);
-    });
+    client
+      .create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
-      <h2 className="head-text">Take a coffee and Chat with Me</h2>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+
       <div className="app__footer-cards">
-        <div className="app__footer-card">
-          <img src={images.mymail} alt="email" />
-          <a href="mailto:ansarishahzaib567@gmail.com" className="p-text">
-            ansarishahzaib567@gmail.com
+        <div className="app__footer-card ">
+          <img src={images.email} alt="email" />
+          <a href="mailto:hello@micael.com" className="p-text">
+            hello@micael.com
           </a>
         </div>
         <div className="app__footer-card">
-          <img src={images.mobile} alt="mobile" />
-          <a href="tel: +923032965359" className="p-text">
-            +923032965359
+          <img src={images.mobile} alt="phone" />
+          <a href="tel:+1 (123) 456-7890" className="p-text">
+            +1 (123) 456-7890
           </a>
         </div>
       </div>
-
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
             <input
               className="p-text"
               type="text"
-              placeholder = "Your Name"
-              name="name"
-              value={name}
+              placeholder="Your Name"
+              name="username"
+              value={username}
               onChange={handleChangeInput}
             />
-        
           </div>
-
           <div className="app__flex">
             <input
               className="p-text"
@@ -86,19 +84,19 @@ const Footer = () => {
           <div>
             <textarea
               className="p-text"
-              name="message"
-              placeholder =  "Your Message"
+              placeholder="Your Message"
               value={message}
+              name="message"
               onChange={handleChangeInput}
             />
           </div>
           <button type="button" className="p-text" onClick={handleSubmit}>
-            {loading ? "Sending" : "Send Message"}
+            {!loading ? "Send Message" : "Sending..."}
           </button>
         </div>
       ) : (
         <div>
-          <h3 className="head-text">Thank you forgetting in touch!</h3>
+          <h3 className="head-text">Thank you for getting in touch!</h3>
         </div>
       )}
     </>
